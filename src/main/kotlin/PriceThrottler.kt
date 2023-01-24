38c36a651a1803226eda159e88c839249411853b
Widowan <Widowan@protonmail.com>
2023-01-24 12:46:49 +0300

    Рефакторинг способа управления потоками
    Раньше потоки запускались просто подряд как пойдут, с условием того что
    дойдут все нужные. Однако теперь я переделал структуру таким образом,
    чтобы одновременно у задачи было всего два потока - текущий активный и
    будущий ещё не активный, который можно заменить в случае поступления
    нового события. Более конкретно: есть мапа валют на пары, вида
    `Процессор : <Текущий поток, Будущий поток>`, где второй меняется или
    запускается (сдвигаясь вместо первого) при необходимости.

--- src/main/kotlin/PriceThrottler.kt
+++ src/main/kotlin/PriceThrottler.kt
@@ -1,23 +1,67 @@
 import kotlinx.coroutines.*
 
-// Tests are included (in respective folder), gradle build is working
+private typealias CurrentAndNextJobs = PriceThrottler.MutablePair<Job?, Job?>
+private typealias ProcessorJobsPair = Pair<PriceProcessor, CurrentAndNextJobs>
+
+// Tests are included (in respective folder) (including new one), gradle build is working
 class PriceThrottler(private val scope: CoroutineScope) : PriceProcessor {
     private val subscribers: MutableList<PriceProcessor>
-    private val ccyJobsMap: MutableMap<String, MutableList<Pair<Job, PriceProcessor>>>
+
+    private val ccyProcessorJobsPairs: MutableMap<String, MutableList<ProcessorJobsPair>>
+
+    data class MutablePair<A, B>(var first: A, var second: B)
 
     init {
         subscribers = ArrayList()
-        ccyJobsMap = HashMap()
+        ccyProcessorJobsPairs = HashMap()
     }
+
+    // For testing purposes
+    constructor() : this(CoroutineScope(Dispatchers.Default + SupervisorJob()))
 
     override fun onPrice(ccyPair: String?, rate: Double) {
         ccyPair?.let { ccy ->
-            val jobList = ccyJobsMap.getOrPut(ccy) { ArrayList() }
-            // It won't stop already running code UNLESS it can be stopped
-            // (i.e. it's another coroutine that is currently suspended), which it is not
-            jobList.onEach { pair -> pair.first.cancel() }.clear()
-            jobList += subscribers.map { scope.launch { it.onPrice(ccy, rate) } to it }
+            subscribers.onEach { delegateJobsProcessing(it, ccy, rate) }
         }
+    }
+
+    private fun nextJob(
+        jobsPair: CurrentAndNextJobs, sub: PriceProcessor, ccy: String, rate: Double,
+        callbackFromJob: Boolean = false
+    ) {
+        if (callbackFromJob) {
+
+            jobsPair.first = jobsPair.second
+            jobsPair.second = null // In case it wasn't null
+            jobsPair.first?.start()
+
+        } else if (jobsPair.first == null && jobsPair.second == null) {
+
+            jobsPair.first = scope.launch {
+                sub.onPrice(ccy, rate)
+                nextJob(jobsPair, sub, ccy, rate, callbackFromJob = true)
+            }
+
+        } else {
+
+            jobsPair.second = scope.launch(start = CoroutineStart.LAZY) {
+                sub.onPrice(ccy, rate)
+                nextJob(jobsPair, sub, ccy, rate, callbackFromJob = true)
+            }
+
+        }
+    }
+
+    private fun delegateJobsProcessing(sub: PriceProcessor, ccy: String, rate: Double) {
+        val jobs = ccyProcessorJobsPairs.getOrPut(ccy) { ArrayList() }
+
+        if (!jobs.any { it.first == sub }) {
+            jobs += ProcessorJobsPair(sub, CurrentAndNextJobs(null, null))
+        }
+
+        jobs.filter { it.first == sub }
+            .map { it.second }
+            .onEach { nextJob(it, sub, ccy, rate) }
     }
 
     override fun subscribe(priceProcessor: PriceProcessor?) {
@@ -25,19 +69,6 @@ class PriceThrottler(private val scope: CoroutineScope) : PriceProcessor {
     }
 
     override fun unsubscribe(priceProcessor: PriceProcessor?) {
-        priceProcessor?.let {
-            subscribers -= it
-            ccyJobsMap.values.onEach { l ->
-                l
-                    .removeAll { pair ->
-                        if (pair.second == it) {
-                            pair.first.cancel()
-                            true
-                        } else {
-                            false
-                        }
-                    }
-            }
-        }
+        priceProcessor?.let { subscribers -= it }
     }
 }
--- src/test/kotlin/PriceThrottlerTest.kt
+++ src/test/kotlin/PriceThrottlerTest.kt
@@ -1,10 +1,14 @@
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.asCoroutineDispatcher
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.atIndex
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.concurrent.Executors
 
+/**
+ * Test you sent me is "onPriceThrottleHard"
+ */
 class PriceThrottlerTest {
     private var subscriberList: MutableList<SlowSubscriber>? = null
     private var priceThrottler: PriceThrottler? = null
@@ -36,7 +40,7 @@ class PriceThrottlerTest {
     }
 
     @Test
-    fun onPriceThrottle() {
+    fun onPriceThrottleSimple() {
         for (s in subscriberList!!) {
             priceThrottler!!.subscribe(s)
         }
@@ -62,7 +66,6 @@ class PriceThrottlerTest {
         }
 
         priceThrottler!!.onPrice("EURUSD", 1.02)
-        Thread.sleep(600)
         priceThrottler!!.unsubscribe(subscriberList!![0])
         priceThrottler!!.unsubscribe(subscriberList!![1])
         priceThrottler!!.onPrice("RUBJPY", 0.72)
@@ -74,19 +77,123 @@ class PriceThrottlerTest {
             .satisfies({ assertThat(it.ccyHistory).doesNotContain("RUBJPY") }, atIndex(0))
             .satisfies({ assertThat(it.ccyHistory).doesNotContain("RUBJPY") }, atIndex(1))
     }
+
+    @Test
+    fun onPriceThrottleManyCcys() {
+        for (s in subscriberList!!) {
+            priceThrottler!!.subscribe(s)
+        }
+
+        var cnt = 0
+        priceThrottler!!.onPrice("AAABBB", (++cnt).toDouble())
+        priceThrottler!!.onPrice("CCCDDD", (++cnt).toDouble())
+        priceThrottler!!.onPrice("EEEFFF", (++cnt).toDouble())
+        priceThrottler!!.onPrice("GGGHHH", (++cnt).toDouble())
+        priceThrottler!!.onPrice("ZZZYYY", (++cnt).toDouble())
+        priceThrottler!!.onPrice("ZZZYYY", (++cnt).toDouble())
+        priceThrottler!!.onPrice("ZZZYYY", (++cnt).toDouble())
+        priceThrottler!!.onPrice("ZZZYYY", (++cnt).toDouble())
+        priceThrottler!!.onPrice("XXX@@@", (++cnt).toDouble())
+        priceThrottler!!.onPrice("SSS!!!", (++cnt).toDouble())
+        priceThrottler!!.onPrice("JJJ###", (++cnt).toDouble())
+
+        Thread.sleep(5000)
+
+        assertThat(subscriberList)
+            .allSatisfy {
+                assertThat(it.ccyHistory).contains(
+                    "AAABBB", "CCCDDD", "EEEFFF", "GGGHHH", "ZZZYYY", "XXX@@@", "SSS!!!", "JJJ###"
+                )
+            }
+    }
+
+    @Test
+    fun onPriceThrottleHardSimplified() {
+        for (s in subscriberList!!) {
+            priceThrottler!!.subscribe(s)
+        }
+
+        var cnt = 0
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // cnt will become 1
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 2
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 3
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 4
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 5
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 6
+        priceThrottler!!.onPrice("DDDFFF", cnt++.toDouble()) // 7
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 8
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 9
+        priceThrottler!!.onPrice("EURUSD", cnt++.toDouble()) // 10
+        priceThrottler!!.onPrice("EURUSD", cnt.toDouble()) // So, 10
+
+        Thread.sleep(2000)
+        assertThat(subscriberList)
+            .allSatisfy { assertThat(it.wasExecuted).isEqualTo(true) }
+            .allSatisfy { assertThat(it.hasFinished).isEqualTo(true) }
+            .allSatisfy { assertThat(it.ccyHistory).contains("EURUSD", "DDDFFF") }
+            .allSatisfy { assertThat(it.rateHistory).contains(0.0, 6.0, 10.0) }
+    }
+
+    @Test
+    fun onPriceThrottleHard() {
+        subscriberList = ArrayList()
+        subscriberList!! += SlowSubscriber(100)
+        subscriberList!! += SlowSubscriber(10)
+        subscriberList!! += SlowSubscriber(1000)
+        for (s in subscriberList!!) {
+            priceThrottler!!.subscribe(s)
+        }
+
+        var r = 0
+        while (r < 30) {
+            priceThrottler!!.onPrice("ARSRUB", r++.toDouble())
+
+            if (r % 10 == 0) {
+                priceThrottler!!.onPrice("DDDFFF", r++.toDouble())
+            }
+
+            Thread.sleep(10)
+        }
+
+        Thread.sleep(5000)
+        assertThat(subscriberList)
+            .allSatisfy { assertThat(it.wasExecuted).isEqualTo(true) }
+            .allSatisfy { assertThat(it.hasFinished).isEqualTo(true) }
+            .allSatisfy { assertThat(it.ccyHistory).contains("ARSRUB", "DDDFFF") }
+            .allSatisfy { assertThat(it.rateHistory).contains(0.0, 10.0, 30.0) }
+    }
+
+    @Test
+    fun unsubscribe() {
+        for (s in subscriberList!!) {
+            priceThrottler!!.subscribe(s)
+        }
+
+        priceThrottler!!.unsubscribe(subscriberList!![0])
+        priceThrottler!!.unsubscribe(subscriberList!![1])
+        priceThrottler!!.onPrice("EURUSD", 1.02)
+
+        Thread.sleep(600)
+        assertThat(subscriberList)
+            .satisfies({ assertThat(it.wasExecuted).isEqualTo(false) }, atIndex(0))
+            .satisfies({ assertThat(it.wasExecuted).isEqualTo(false) }, atIndex(1))
+    }
 }
 
-private class SlowSubscriber : PriceProcessor {
+private class SlowSubscriber(val sleep: Long) : PriceProcessor {
     var wasExecuted = false
     var hasFinished = false
     val ccyHistory = mutableListOf<String>()
     val rateHistory = mutableListOf<Double>()
 
+    constructor() : this(100)
+
     override fun onPrice(ccyPair: String?, rate: Double) {
         wasExecuted = true
         ccyHistory += ccyPair!!
         rateHistory += rate
-        Thread.sleep(100)
+        Thread.sleep(sleep)
+        println("I am $this, got: $ccyPair:$rate")
         hasFinished = true
     }
 
@@ -97,4 +204,10 @@ private class SlowSubscriber : PriceProcessor {
     override fun unsubscribe(priceProcessor: PriceProcessor?) {
         TODO("Unreachable")
     }
+
+    override fun toString(): String =
+        "SlowSubscriber[" +
+                this.sleep.toString().padEnd(4, ' ') +
+                "]@" +
+                this.hashCode().toString(16).padStart(8, '0')
 }
